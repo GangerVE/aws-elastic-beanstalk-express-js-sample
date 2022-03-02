9fbc88f186854e5b4b3d188c52331b77beeaf823
Juan Guerra <jguerra0410@gmail.com>
2022-03-02 13:41:00 -0300

    Cambio 3

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Maldito Falso!'));
-app.get('/', (req, res) => res.send('Te amo Isabelita Brito <3'));
+app.get('/', (req, res) => res.send('Hello Maldito Falso! Te amo Isabel'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
